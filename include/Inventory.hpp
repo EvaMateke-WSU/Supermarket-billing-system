3172dcc17aaae5f32ef4d822d04af222af50e33c
Eva Mateke <matekeeva0@gmail.com>
2026-09-01 18:02:37 +0200

    Add Inventory class with basic functionality

---
+++ include/Inventory.hpp
@@ -0,0 +1,17 @@
+#ifndef INVENTORY_HPP
+#define INVENTORY_HPP
+
+#include "Product.hpp"
+#include <vector>
+
+class Inventory {
+private:
+    std::vector<Product> stock;
+
+public:
+    void addStock(const Product& p);
+    void displayStock() const;
+    Product findProduct(int id) const;
+};
+
+#endif
