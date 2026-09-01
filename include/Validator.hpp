fa67d310d0803a97b2f629fdecda8a12b412a2c7
Eva Mateke <matekeeva0@gmail.com>
2026-09-01 17:58:57 +0200

    Add Validator class with price and quantity validation

---
+++ include/Validator.hpp
@@ -0,0 +1,10 @@
+#ifndef VALIDATOR_HPP
+#define VALIDATOR_HPP
+
+class Validator {
+public:
+    static bool isValidPrice(double price);
+    static bool isValidQuantity(int quantity);
+};
+
+#endif
